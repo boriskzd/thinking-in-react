acccacb076774b7487e3e9b26537a1977738b281
boriskzd <boriskzd@users.noreply.github.com>
2023-09-11 16:39:24 +0200

    create categories object with array of products and display it

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,54 +1,36 @@
 import ProductRow from "./ProductRow";
 import ProductCategoryRow from "./ProductCategoryRow";
+import React from "react";
 
 export default function ProductTable({ products }) {
-	console.log("products: ", products);
-
 	// Fruits, Vegetables
-
 	const categories = {};
 
+	// create data object
 	products.forEach((item) => {
-		// console.log(groups);
-		// console.log(item);
-		const { category, ...rest } = item;
-		console.log(category, rest);
+		const { category } = item;
 
 		if (!categories[category]) {
-			console.log(" there is no ", categories[category]);
 			categories[category] = [];
-
-			console.log(categories[category]);
-		}
-
-		{
-			// categories[category];
-			console.warn(categories[category]);
 		}
 
 		categories[category].push(item);
 	});
 
-	console.log("groupedItems - categories");
-	console.log(categories);
-
-	// chat gpt code
-
-	const rows = [];
-	let lastCategory = null;
-
-	products.forEach((product) => {
-		if (product.category !== lastCategory) {
-			rows.push(
-				<ProductCategoryRow
-					category={product.category}
-					key={product.category}
-				/>
-			);
-		}
-		rows.push(<ProductRow product={product} key={product.name} />);
-		lastCategory = product.category;
-	});
+	const Table = ({ categories }) => {
+		return (
+			<>
+				{Object.keys(categories).map((category) => (
+					<React.Fragment key={category}>
+						<ProductCategoryRow category={category} />
+						{categories[category].map((item) => (
+							<ProductRow product={item} key={item.name} />
+						))}
+					</React.Fragment>
+				))}
+			</>
+		);
+	};
 
 	return (
 		<table>
@@ -58,16 +40,7 @@ export default function ProductTable({ products }) {
 					<th>Price</th>
 				</tr>
 			</thead>
-			<tbody>
-				{<hr />}
-				{rows}
-				{/* {<hr />} */}
-				{/* {rowsFinal} */}
-				{/* {<hr />}
-				{products.map((rows) => (
-					<ProductRow product={rows} />
-				))} */}
-			</tbody>
+			<tbody>{<Table categories={categories} />}</tbody>
 		</table>
 	);
 }
